19631f7d706ec32894751cb4df344173edfd7b20
AlfredSchou <abjarneg@live.dk>
2024-11-15 11:20:35 +0100

    undervisning 151124

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const[users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch ("https://randomuser.me/api/?results=5")
+            .then (res => res.json())
+            .then(data => setUsers(data.results))
+    },[])
+ 
+    return (
+        <div>
+            <p>
+                {users.map( u=> <p>{u.name.first }, age {u.dob.age}</p>)}.
+
+            </p>
+        </div>
+             
+    );
 }
 
 export default App;
